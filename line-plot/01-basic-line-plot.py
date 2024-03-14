885016577641413b48791ceba566e13c6e282d15
nipuran <nipuransarania@gmail.com>
2024-03-14 19:58:20 +0530

    Initial commit

--- line-plot/01-basic-line-plot.py
+++ line-plot/01-basic-line-plot.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 
 # datas
 ages = [24, 25, 26, 27, 28]
-salaries = [50238, 82784, 48325, 49281, 20130]
+salaries = [28743, 53723, 40325, 43278, 50842]
 
 # creating the plot
 plt.plot(ages, salaries)
@@ -13,7 +13,7 @@ plt.xlabel('Ages')
 plt.ylabel('Salaries')
 
 # name of the plot
-plt.title('Salaries by Ages')
+plt.title('Ages by Salaries')
 
 # show the plot
 plt.show()

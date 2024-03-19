c5789f7996bda57d02459501ad8efa9a7e49d440
nipuran <nipuransarania@gmail.com>
2024-03-19 22:23:54 +0530

    Initial commit

---
+++ sub-plot/02-multi-fig.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+# Age,All_Devs,Python,JavaScript
+
+age = df['Age']
+all_devs = df['All_Devs']
+python = df['Python']
+javascript = df['JavaScript']
+
+fig, ax1 = plt.subplots()
+fig, ax2 = plt.subplots()
+
+ax1.plot(age, python, label='Python')
+ax1.plot(age, javascript, label='JavaScript')
+ax2.plot(age, all_devs, linestyle='--', label='All Devs')
+
+ax1.legend()
+ax1.set_title('Salaries by Ages')
+ax1.set_xlabel('Ages')
+ax1.set_ylabel('Salaries')
+
+ax2.legend()
+ax2.set_title('Salaries by Ages')
+ax2.set_ylabel('Salaries')
+ax2.set_xlabel('Ages')
+
+plt.tight_layout()
+plt.show()
